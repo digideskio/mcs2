8865a3f17bb467107ec0d8fd9cd8a7d4572f48dd
Nathan Hagen <panzerschreck1@gmail.com>
2011-05-17 15:42:48 -0400

    Default rails installation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcs2::Application.config.secret_token = '7ec6e5d9a957547b787ad32f827cbbe9bf894ecd5ce1f581a61c959bc3d991739de92ede24f6d6862daeb62770ac94276e36f569c2ad4947b10ffaaa5ec43c44'
