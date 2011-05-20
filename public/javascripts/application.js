faefd71eaba29a5da79a00e64e10811d673d8d8b
Nathan Hagen <panzerschreck1@gmail.com>
2011-05-20 13:26:22 -0400

    more style changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+
+  $("ul#serverlist-ul > li").click(function(e) {
+      window.location.href = 'servers/'+$(this).attr("id");
+  })
+  .find("a")
+  .click(function(e) {
+      e.stopPropagation();
+  });
+
+
+
+});
